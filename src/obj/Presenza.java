dcf1aedde47e5aea1782b6060618ecc9c56764a6
alexx08green <alexxandro.izzo@gmail.com>
2025-05-13 13:20:48 +0200

    v 2.0 aggiunta e correzione dei metodi get, aggiunta dei metodi set

--- src/obj/Presenza.java
+++ src/obj/Presenza.java
@@ -2,22 +2,38 @@ package obj;
 import java.time.LocalDate;
 
 public class Presenza {
-    public class Stato {
-        private LocalDate giorno;
-        private char stato;
+    //VARIABILI D'ISTANZA
+    //data
+    private LocalDate giorno;
+    //stato dello studente (A = ASSENTE, P = PRESENTE)
+    private char stato;
 
-        public Stato (LocalDate p_giorno, char p_stato) {
-            this.giorno = p_giorno;
-            this.stato = p_stato;
-        }
-
-        public LocalDate getGiorno() {
-            return giorno;
-        }
-
-        public char getStato() {
-            return stato;
-        }
+    //METODO COSTRUTTORE
+    public Presenza (LocalDate p_giorno, char p_stato) {
+        //giorno/data
+        this.giorno = p_giorno;
+        //stato dello studente
+        this.stato = p_stato;
     }
 
+    //METODI D'ISTANZA
+    //METODI GET
+    //get giorno
+    public LocalDate getGiorno() {
+        return this.giorno;
+    }
+    //get stato
+    public char getStato() {
+        return this.stato;
+    }
+
+    //METODI SET
+    //set girno
+    public void setGiorno(LocalDate giorno) {
+        this.giorno = giorno;
+    }
+    //set stato
+    public void setStato(char p_stato) {
+        this.stato = p_stato;
+    }
 }
