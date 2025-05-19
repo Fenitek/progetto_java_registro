97339c4fe19f693680e65c477131fb85d03b4c56
alexx08green <alexxandro.izzo@gmail.com>
2025-05-19 12:34:33 +0200

    v 1.1 perfezionati metodi classe Materie in packge utility

--- src/utility/Materie.java
+++ src/utility/Materie.java
@@ -3,10 +3,10 @@ package utility;
 public class Materie {
     //VARIABILI
     //arrray di indici
-    String[] nomeMateria = {"Italinao","Matematica","Informatica","Ginnastica","Storia","Inglese","Educaizone civica","Tpsit","Sistemi e reti","Telecomunicazioni"};
+    public static String[] nomeMateria = {"Italinao","Matematica","Informatica","Ginnastica","Storia","Inglese","Educaizone civica","Tpsit","Sistemi e reti","Telecomunicazioni"};
 
     //metodo che cerca l'indice in base alla classe
-    public int indiceMateria (String p_materia) {
+    public static int indiceMateria (String p_materia) {
         //indice che ritornerò, se è uguale a -1 vuoldire che non ho trovato la materia, lo comunicherò all'utente
         int indice = -1;
 
