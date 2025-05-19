925314ec36fe83113f85d88572a165986c3d52a1
alexx08green <alexxandro.izzo@gmail.com>
2025-05-19 12:42:05 +0200

    v 1.3 aggiunga metodo che in base all'indice da il nome della materia. può servire per sapere in base alla riga della matrice voti di che materia si tratta.

--- src/utility/Materie.java
+++ src/utility/Materie.java
@@ -3,24 +3,32 @@ package utility;
 public class Materie {
     //VARIABILI
     //arrray di indici
-    public static String[] nomeMateria = {"Italinao","Matematica","Informatica","Ginnastica","Storia","Inglese","Educaizone civica","Tpsit","Sistemi e reti","Telecomunicazioni"};
+    public static String[] arrayMateria= {"Italinao","Matematica","Informatica","Ginnastica","Storia","Inglese","Educaizone civica","Tpsit","Sistemi e reti","Telecomunicazioni"};
 
-    //metodo che cerca l'indice in base alla classe
+    //metodo che cerca l'indice in base alla materia
     public static int indiceMateria (String p_materia) {
         //indice che ritornerò, se è uguale a -1 vuoldire che non ho trovato la materia, lo comunicherò all'utente
         int indice = -1;
 
         //variabile di controllo se ho trovato
         boolean trovato = false;
-        for (int i = 0; i < nomeMateria.length && !trovato; i++){
+        for (int i = 0; i < arrayMateria.length && !trovato; i++){
             //contorllo ingorando maiuscole e minusocle la stringa data come parametro e quella nell'array
-            if (nomeMateria[i].equalsIgnoreCase(p_materia)) {
+            if (arrayMateria[i].equalsIgnoreCase(p_materia)) {
                 trovato = true;
                 indice = i;
             }
         }
 
         return indice;
+    }
+
+    //metodo che cerca la materia in base all'indice, può servire per cercare voti di determinate materie sapendo l'indice
+    public static String nomeMateria (int p_indice) {
+        //prendo indice materia partendo dalla sringa
+        String nomeM = arrayMateria[p_indice];
+
+        return nomeM;
     }
 
 }
