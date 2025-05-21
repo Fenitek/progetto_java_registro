b310158683aaa4439d12588465715ac73f075534
alexx08green <alexxandro.izzo@gmail.com>
2025-05-21 11:32:07 +0200

    v 2.1 aggiunta e sistemaizone variabili e metodi get

--- src/obj/Studente.java
+++ src/obj/Studente.java
@@ -7,7 +7,8 @@ import utility.*;
 public class Studente {
 	//VARIABILI D'ISTANZA
 	//nome e cognome
-	private String nomeCognome;
+	private String nome;
+	private String cognome;
 	//data di nascita
 	private LocalDate dataNascita;
 	//array di presenze
@@ -23,6 +24,10 @@ public class Studente {
 
 	//METODO COSTRUTTORE
 	public Studente(String name, String surname, LocalDate p_data) {
+		//nome
+		this.nome = name;
+		//cognome
+		this.cognome = surname;
 		//data di nascita
 		this.dataNascita = p_data;
 		//array oggetto presenze
@@ -36,8 +41,11 @@ public class Studente {
 	//METODI D'ISTANZA
 	//METODI GET
 	//get nomecognome
-	public String getNomeCognome() {
-		return this.nomeCognome;
+	public String getNome() {
+		return this.nome;
+	}
+	public String getCognome() {
+		return this.cognome;
 	}
 	//get data nascita
 	public LocalDate getDataNascita() {
